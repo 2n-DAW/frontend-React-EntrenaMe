e98bfdbc1dc64e228f7389d4cb73a2b0717cc308
Llorens19 <llorenssorianodiego@gmail.com>
2024-11-29 01:28:00 +0100

    Cambios estéticos

--- src/components/lists/SportList.tsx
+++ src/components/lists/SportList.tsx
@@ -5,14 +5,15 @@ const SportList = () => {
     const { sports } = useSports();
     return (
         <>
-            <h2 className="text-2xl font-bold text-deep-orange-600 mb-4">Deportes</h2>
+            <section className="sport_list p-4">
+                <h2 className="text-2xl font-bold text-deep-orange-600 mb-4">Deportes</h2>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-                {sports.map((sport) => (
-                    console.log(sport),
-                    <SportCard key={sport.id_sport}  data = {sport}/>
-                ))}
-            </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                    {sports.map((sport) => (
+                        <SportCard key={sport.id_sport} data={sport} />
+                    ))}
+                </div>
+            </section>
         </>
     );
 }
