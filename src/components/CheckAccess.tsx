b4664a3cec8d44ea48ec5b5562b7b78fdf29b819
Llorens19 <llorenssorianodiego@gmail.com>
2024-12-08 13:11:16 +0100

    Componente CheckAccess funcionando a falta de petición al backend

---
+++ src/components/CheckAccess.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+
+
+interface AccessCheckerProps {
+    children: React.ReactNode;
+}
+
+const CheckAccess: React.FC<AccessCheckerProps> = ({ children }) => {
+    const [hasAccess, setHasAccess] = useState<boolean>(false);
+
+    useEffect(() => {
+
+        console.log('Verificando acceso');
+        setHasAccess(false); 
+    }, []);
+
+    if (hasAccess === false) {
+        return <div>No tienes acceso</div>;
+    } else {
+        return <>{children}</>;
+    }
+};
+
+export default CheckAccess;
